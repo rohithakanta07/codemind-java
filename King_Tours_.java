6d3db8f814644830ee796333d00d134c366e2b31
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2024-03-04 10:15:15 +0530

    import java.util.*; public class KingTours{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int m = sc.nextInt();         int a = (n*5)+(m*7);         System.out.println(a);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class KingTours{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a = (n*5)+(m*7);
+        System.out.println(a);
+    }
+}
