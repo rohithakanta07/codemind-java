a4083b73319df314a9b070e3db5815253c6078f7
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2024-03-04 11:25:43 +0530

    import java.util.*; public class ProfitPercentage{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int area = a*a;         int peri = 4*a;         System.out.println(area+" "+peri);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ProfitPercentage{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int area = a*a;
+        int peri = 4*a;
+        System.out.println(area+" "+peri);
+    }
+}
